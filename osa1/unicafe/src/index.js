2b1176dbce8d38225fe855203d1f9077a7a95fa6
Heliozoa <dm89132@gmail.com>
2020-01-25 08:24:51 +0200

    1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -38,14 +38,21 @@ const Feedback = ({ good, neutral, bad, all, average, positive, setGood, setNeut
 }
 
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+    if (all > 0) {
+        return <>
+            <h2>statistics</h2>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+            <div>all {all}</div>
+            <div>average {average}</div>
+            <div>positive {positive}%</div>
+        </>
+    }
+
     return <>
         <h2>statistics</h2>
-        <div>good {good}</div>
-        <div>neutral {neutral}</div>
-        <div>bad {bad}</div>
-        <div>all {all}</div>
-        <div>average {average}</div>
-        <div>positive {positive}%</div>
+        <div>No feedback given</div>
     </>
 }
 
