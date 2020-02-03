1b0b26829d55a3167c97e40cb81d42d4dff79e46
Heliozoa <dm89132@gmail.com>
2020-02-03 19:16:08 +0200

    2.15

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -22,7 +22,9 @@ const App = () => {
     if (persons.map(p => p.name).includes(newName)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({ name: newName, number: newNumber }))
+      const newPerson = { name: newName, number: newNumber }
+      axios.post('http://localhost:3001/persons', newPerson)
+      setPersons(persons.concat(newPerson))
     }
   }
 
