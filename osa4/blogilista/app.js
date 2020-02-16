20b20ab0be647c1ff5d555db34a5cdaca95513b3
Heliozoa <dm89132@gmail.com>
2020-02-16 11:38:11 +0200

    4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+const blogsRouter = require('./controllers/notes')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+const mongoUrl = config.MONGODB_URI
+const app = express()
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,44 +1,8 @@
-const config = require('./config')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
 
 const port = config.PORT
-const mongoUrl = config.MONGODB_URI
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
