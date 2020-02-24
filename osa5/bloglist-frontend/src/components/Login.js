96d6a021b6eef8d448e18878f55a4de1f2700666
Heliozoa <dm89132@gmail.com>
2020-02-24 10:45:15 +0200

    5.1

---
+++ osa5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = ({ login, setUsername, setPassword }) => {
+    return <div>
+        <h2>log in to application</h2>
+        <form onSubmit={event => { event.preventDefault(); login() }}>
+            <div>username <input onChange={event => setUsername(event.target.value)} /></div>
+            <div>password <input onChange={event => setPassword(event.target.value)} /></div>
+            <div><button type="submit">login</button></div>
+        </form>
+    </div >
+}
+
+export default Login
---
+++ osa5/bloglist-frontend/src/services/login.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const baseUrl = '/api/login'
+
+const login = async (username, password) => {
+    try {
+        const response = await axios.post(baseUrl, { username, password })
+        return response.data
+    } catch (err) {
+        console.error(err.response.data.error)
+        return null
+    }
+}
+
+export default { login }
