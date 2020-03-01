dc3acd66f585b4a9bb7a9add06915ebdaf192590
Heliozoa <dm89132@gmail.com>
2020-03-01 15:50:49 +0200

    6.13

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import { init } from './reducers/anecdoteReducer'
+import { getAll } from './services/anecdotes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    getAll().then(anecdotes => dispatch(init(anecdotes)))
+  })
 
   return (
     <div>
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,24 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
 export const vote = (id) => {
   console.log(id)
   return {
@@ -35,7 +14,14 @@ export const create = (anecdote) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+export const init = (anecdotes) => {
+  return {
+    type: 'INIT',
+    data: { anecdotes }
+  }
+}
+
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -52,9 +38,10 @@ const reducer = (state = initialState, action) => {
       return newState
     case 'CREATE':
       const text = action.data.anecdote
-      const obj = asObject(text)
-      console.log('now', [...state, obj])
-      return [...state, obj]
+      return state
+    case 'INIT':
+      const anecdotes = action.data.anecdotes
+      return anecdotes
     default:
       return state
   }
