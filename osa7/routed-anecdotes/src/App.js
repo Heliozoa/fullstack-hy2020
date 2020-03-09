ccda40e43c91bd509f2649d2471669f47266d875
Heliozoa <dm89132@gmail.com>
2020-03-09 12:43:05 +0200

    7.6

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -62,11 +62,10 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
   const history = useHistory()
-
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -87,9 +86,9 @@ const CreateNew = (props) => {
 
   const reset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
