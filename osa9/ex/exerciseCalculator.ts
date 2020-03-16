ae9d7343f02a10f51679bc833be6e836a45675fb
Heliozoa <dm89132@gmail.com>
2020-03-16 14:28:03 +0200

    9.2

---
+++ osa9/ex/exerciseCalculator.ts
@@ -0,0 +1,41 @@
+interface ExerciseResult {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number,
+}
+
+const calculateExercises = (hours: Array<number>, target: number): ExerciseResult => {
+    const periodLength = hours.length;
+    const trainingDays = hours.filter(h => h != 0).length;
+    const success = hours.map(h => h >= target).reduce((acc, curr) => acc && curr);
+    const average = hours.reduce((acc, curr) => acc + curr) / hours.length;
+
+    let rating;
+    let ratingDescription;
+    if (success) {
+        rating = 3;
+        ratingDescription = "you met the target!";
+    } else if (trainingDays > periodLength * 0.8) {
+        rating = 2;
+        ratingDescription = "you almost met the target";
+    } else {
+        rating = 1;
+        ratingDescription = "you did not meet the target";
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average,
+    };
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
