594629be7ebe0884a78d8153ee84532bb8410bbe
Heliozoa <dm89132@gmail.com>
2020-03-16 15:51:12 +0200

    9.5

--- osa9/ex/index.ts
+++ osa9/ex/index.ts
@@ -1,10 +1,29 @@
 import express from 'express';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
 
 app.get('/hello', (_, res) => {
     res.send('Hello Full Stack!');
 });
+
+app.get('/bmi', (req, res) => {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+
+    if (height && weight) {
+        const bmi = calculateBmi(height, weight);
+        res.json({
+            weight,
+            height,
+            bmi,
+        });
+    } else {
+        res.json({
+            error: 'malformatted parameters',
+        });
+    }
+})
 
 const PORT = 3003;
 
