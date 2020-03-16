623602dd8cd9d8e98eb07a6ab73a7aacd9eae819
Heliozoa <dm89132@gmail.com>
2020-03-16 21:34:17 +0200

    9.9

--- osa9/patientor/index.ts
+++ osa9/patientor/index.ts
@@ -3,12 +3,12 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/ping', (_, res) => {
+app.get('/api/ping', (_, res) => {
     console.log('ping');
     res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log('running');
 });
