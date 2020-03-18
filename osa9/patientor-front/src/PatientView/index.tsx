876ff2b10909287893eb5c78b577584e84662b42
Heliozoa <dm89132@gmail.com>
2020-03-18 13:17:00 +0200

    9.21

--- osa9/patientor-front/src/PatientView/index.tsx
+++ osa9/patientor-front/src/PatientView/index.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Patient, Gender } from "../types";
+import { Patient, Gender, Diagnosis, Entry } from "../types";
 import { RouteComponentProps } from 'react-router-dom';
 import { apiBaseUrl } from "../constants";
 import axios from "axios";
 import { useStateValue } from '../state';
 import { updatePatient } from '../state/reducer';
+import { isArray } from 'util';
+
+type DiagnosisMap = { [id: string]: Diagnosis } | undefined | null;
 
 const PatientView: React.FC<RouteComponentProps<{ id: string | undefined }>> = ({ match }) => {
     const id = match.params.id;
@@ -13,7 +16,7 @@ const PatientView: React.FC<RouteComponentProps<{ id: string | undefined }>> = (
     }
 
     const [state, dispatch] = useStateValue();
-    const [patient, setPatient] = useState<Patient | undefined | null>();
+    const [patient, setPatient] = useState<Patient | undefined | null>(undefined);
 
     useEffect(() => {
         const existing = state.patients[id];
@@ -47,28 +50,87 @@ const PatientView: React.FC<RouteComponentProps<{ id: string | undefined }>> = (
         </div>;
     }
 
-    let gender;
+    const genderAbbr = (gender: Gender) => {
+        let genderAbbr;
 
-    switch (patient.gender) {
-        case Gender.Male:
-            gender = "M";
-            break;
-        case Gender.Female:
-            gender = "F";
-            break;
-        case Gender.Other:
-            gender = "O";
-            break;
-        default:
-            const n: never = patient.gender;
-            break;
-    };
+        switch (gender) {
+            case Gender.Male:
+                genderAbbr = "M";
+                break;
+            case Gender.Female:
+                genderAbbr = "F";
+                break;
+            case Gender.Other:
+                genderAbbr = "O";
+                break;
+            default:
+                const n: never = gender;
+                break;
+        };
+
+        return genderAbbr;
+    }
 
     return <div>
-        <h2>{patient.name} [{gender}]</h2>
+        <h2>{patient.name} [{genderAbbr(patient.gender)}]</h2>
         <div>ssn: {patient.ssn}</div>
         <div>occupation: {patient.occupation}</div>
+        <h3>entries</h3>
+        <Entries entries={patient.entries} />
     </div>
+}
+
+interface EntriesProps {
+    entries: Entry[];
+}
+
+const Entries = ({ entries }: EntriesProps) => {
+    const [diagnoses, setDiagnoses] = useState<DiagnosisMap>(undefined);
+
+    useEffect(() => {
+        axios.get<Diagnosis[] | Error>(`${apiBaseUrl}/diagnoses`).then(({ data }) => {
+            if (isArray(data)) {
+                setDiagnoses(data.reduce<{ [code: string]: Diagnosis }>((map, diag) => {
+                    map[diag.code] = diag;
+                    return map;
+                }, {}))
+            } else {
+                setDiagnoses(null);
+            }
+        }).catch(err => {
+            setDiagnoses(null);
+        });
+    }, [])
+
+    return <>{entries.map(e => {
+        return <div key={e.id}>
+            <div>{e.date} {e.description}</div>
+            <ul>
+                <EntryView entry={e} diagnoses={diagnoses} />
+            </ul>
+        </div>
+    })}</>
+}
+
+interface EntryViewProps {
+    entry: Entry,
+    diagnoses: DiagnosisMap,
+}
+
+const EntryView = ({ entry, diagnoses }: EntryViewProps) => {
+    const getDiagnosisName = (code: string, diagnoses: DiagnosisMap): string => {
+        if (!diagnoses || !(code in diagnoses)) {
+            return '';
+        }
+
+        return diagnoses[code].name;
+    }
+
+    return <>{
+        entry.diagnosisCodes?.map(c => {
+            return <li key={c}>{c} {getDiagnosisName(c, diagnoses)}</li>
+        })
+    }</>
 }
 
 export default PatientView;
--- osa9/patientor-front/src/types.ts
+++ osa9/patientor-front/src/types.ts
@@ -1,3 +1,33 @@
+export type Entry = OccupationalHealthCareEntry | HospitalEntry | HealthCheckEntry;
+
+interface BaseEntry {
+  id: string;
+  date: string;
+  type: string;
+  specialist: string;
+  diagnosisCodes?: string[];
+  description: string;
+}
+
+export interface OccupationalHealthCareEntry extends BaseEntry {
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  discharge: {
+    date: string,
+    criteria: string,
+  };
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  healthCheckRating: number;
+}
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -17,6 +47,7 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 };
 
 export interface Error {
